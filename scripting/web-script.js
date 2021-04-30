0b004efedee55971b62c7f9b85580db89f8930d9
Hiperultimate <hiperultimatevegito@gmail.com>
2021-05-01 00:44:36 +0530

    Made main project page

--- scripting/web-script.js
+++ scripting/web-script.js
@@ -1,19 +1,15 @@
 const mobileBtn = document.getElementById("menu-button")
     nav = document.querySelector('nav')
     mobileBtnExit = document.getElementById("exit-menu-button");
+    gradButton = document.getElementsByClassName("gradient-button");
 ;
 
     
 mobileBtn.addEventListener('click', () => {
-    // nav.classList.add('menu-btn');
-    // nav.style.width = "80%";
     nav.style.right = "0%" ;
-
 });
-mobileBtnExit.addEventListener('click', () => {
-    // nav.classList.remove('menu-btn');
-    // nav.style.width = "0px";
-    nav.style.right = "-80%" ;
 
+mobileBtnExit.addEventListener('click', () => {
+    nav.style.right = "-80%" ;
 });
 
