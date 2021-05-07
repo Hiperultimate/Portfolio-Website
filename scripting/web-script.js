f78266e40251866908001d7d42c0d9e6f2be95ed
Hiperultimate <hiperultimatevegito@gmail.com>
2021-05-08 02:17:57 +0530

    Made Navbar follow throughout the page

--- scripting/web-script.js
+++ scripting/web-script.js
@@ -2,8 +2,12 @@ const mobileBtn = document.getElementById("menu-button")
     nav = document.querySelector('nav')
     mobileBtnExit = document.getElementById("exit-menu-button");
     gradButton = document.getElementsByClassName("gradient-button");
+    
 ;
 
+var nav_bar = document.getElementById('navbar')
+menu_button = document.getElementById("menu-button")
+cover_page = document.getElementById('cover-page');
     
 mobileBtn.addEventListener('click', () => {
     nav.style.right = "0%" ;
@@ -14,3 +18,22 @@ mobileBtnExit.addEventListener('click', () => {
     nav.style.right = "-80%" ;
 });
 
+window.addEventListener("scroll", function(){
+    if(window.scrollY >= cover_page.offsetHeight){
+        nav_bar.style.position = "fixed";
+        nav_bar.style.width ="100%";
+        menu_button.style.paddingRight = "2em";
+        cover_page.style.paddingTop ="84px";
+        nav_bar.style.borderBottom = "2px solid white";
+    }
+
+    if(window.scrollY < cover_page.offsetHeight){
+        nav_bar.style.position = "relative";
+        nav_bar.style.width ="auto";
+        menu_button.style.paddingRight = "0em";
+        cover_page.style.paddingTop ="0px";
+        nav_bar.style.borderBottom = "0px solid white";
+    }
+})
+
+console.log(cover_page.offsetHeight);
