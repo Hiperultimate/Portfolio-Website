18327f7cc89c3db195e0cef0d213c8a047357b51
Hiperultimate <hiperultimatevegito@gmail.com>
2021-05-15 14:54:46 +0530

    Switching to normal animation with this logic

--- Loading Animation/app.js
+++ Loading Animation/app.js
@@ -1,14 +1,44 @@
 var logo = document.getElementById("logo-loading");
 var stroke = document.getElementsByClassName("stroke");
 
-function loadingAnimationEnd(){
-    logo.classList.add("logo-loadingEnd");
-    for(var i = 0 ; i < stroke.length ; i++){
-        stroke[i].style.strokeDasharray = "800";
+var stop_animation = 1;
+
+function alternate_animate(dashoffset){
+    for(var i = 0; i < stroke.length; i++){
+        stroke[i].style.strokeDashoffset = dashoffset;
     }
+    if(stop_animation == 0){
+        for(var i = 0; i < stroke.length; i++){
+            console.log(window.getComputedStyle(stroke[i]).strokeDasharray);
+            stroke[i].style.strokeDashoffset = 0;
+            stroke[i].style.strokeDasharray = 800;
+        }
+        return
+    }
+    if(dashoffset == 800){
+        dashoffset = 0;
+    } else {
+        dashoffset = 800;
+    }
+    setTimeout(function(){
+        alternate_animate(dashoffset)
+    },1000)
 }
 
+alternate_animate(800)
 
-console.log("Starting timer");
-setTimeout(loadingAnimationEnd, 3000);
-console.log("operation finished");
+setTimeout(function(){
+    stop_animation = 0;
+},2300)
+
+// function loadingAnimationEnd(){
+//     logo.classList.add("logo-loadingEnd");
+//     for(var i = 0 ; i < stroke.length ; i++){
+//         stroke[i].style.strokeDasharray = "800";
+//     }
+// }
+
+
+// console.log("Starting timer");
+// setTimeout(loadingAnimationEnd, 2500);
+// console.log("operation finished");
